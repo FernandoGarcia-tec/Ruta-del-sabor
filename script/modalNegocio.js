db81a6eacd7deb0d7c3ffd2b5f14ec5e99d58671
Fernando <garciaquintero00@gmail.com>
2024-10-26 23:13:27 -0600

    mensajes

--- script/modalNegocio.js
+++ script/modalNegocio.js
@@ -5,9 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(data => {
             console.log('API response data:', data); // Log the entire data object
-
+            const cardNegocios = document.getElementById('cardNegocios');
+            const Mensaje = document.getElementById('Mensaje');
+            const MensajeLimite = document.getElementById('MensajeLimite');
             if (data.length === 0) {
                 console.error('No se encontraron negocios');
+                cardNegocios.style.display = 'none'; // Hide the card if there are no businesses
+                Mensaje.style.display = 'block'; // Show the message if there are no businesses
+                MensajeLimite.style.display = 'none'; // Hide the message if there are no businesses
                 return;
             }
             
@@ -32,9 +37,11 @@ document.addEventListener("DOMContentLoaded", function() {
             const showmen = document.getElementById("showmen");
             const modalAñadir = document.getElementById("openModalBtn");
             if (data.length > 1) {
-               
+               cardNegocios.style.display = 'block';
                 showmen.style.display = "block";
                 modalAñadir.style.display = "none";
+                Mensaje.style.display = 'none';
+                MensajeLimite.style.display = 'block';
 
 
             }
@@ -43,3 +50,4 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Error:', error));
 });
+
